a83c7a85b1d116c62f012374c7b55ae909559e31
seanlee1379@gmail.com <seanlee1379@gmial.com>
2022-03-16 10:23:54 -0700

    moreedits to js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var userInput = $("#userInput");
+var homeBtn = $("#searchBtn")
 var apiKey = "2849398c70c43740b834e55dce0bad4a";
 var pastCities = []
 
@@ -6,7 +7,7 @@ console.log(userInput)
 
 function cityWeather () {
     var userRequest = localStorage.getItem("userInput")
-    var weatherAPI = "api.openweathermap.org/data/2.5/weather?q" + userInput + "&appid=" + apiKey;
+    var weatherAPI = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput.val() + "&appid=" + apiKey;
 
     fetch(weatherAPI)
     .then(function (response) {
@@ -17,11 +18,37 @@ function cityWeather () {
         }
       })
       .then(function (data) {
-        console.log(data);
+        //console.log(data);
+        var lat = data.coord.lat
+        var lon = data.coord.lon
+        fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&units=imperial&appid=" + apiKey)
+        .then(function (response) {
+            if (response.status === 404) {
+              console.log(response);
+            } else {
+              return response.json();
+            }
+          })
+          .then(function (data) {
+            console.log(data);
+
+    
+    
+    
+    
+    
+            
+          });
+
+
+
+
         
       });
 }
 
 function setUserInput() {
     localStorage.setItem("userInput", userInput.val().replace(/\s+/g, "+"));
-  }
+}
+
+homeBtn.on("click", cityWeather)
